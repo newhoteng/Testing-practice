71a8e0f7976c7b267ea5d177f3fef044c981918f
Harriet Oteng <harriet.oteng@gmail.com>
2023-05-06 14:39:27 +0000

    Add commented out react testing for user interactions

--- task1.test.js
+++ task1.test.js
@@ -13,3 +13,14 @@ test('', () => {
   // expect(() => stringLength('')).toThrow();
   expect(() => stringLength('')).toThrow(Error);
 });
+
+// React testing library, try it out later
+// import { render, fireEvent } from '@testing-library/react';
+//  it('should update the screen on button click', () => {
+//     const { getByTestId, getByText } = render(<Calculator />);
+//     fireEvent.click(getByText('2'));
+//     fireEvent.click(getByText('+'));
+//     fireEvent.click(getByText('3'));
+//     fireEvent.click(getByText('='));
+//     expect(getByTestId('screen')).toHaveTextContent('5');
+//   });
